e4343bc621ed51302677ef427e73e8675fbf2cc8
timichal <michal@zlatkovsky.cz>
2019-06-22 09:56:16 +0200

    kalkulacka

--- article.md
+++ article.md
@@ -6,8 +6,22 @@ styles: []
 libraries: [] #jquery, d3, highcharts, datatables
 options: [noheader] #wide, noheader (, nopic)
 ---
+Klimatická změna je čím dál častějším tématem ve společnosti. Odborníci varují, probíhají klimatické články, v Česku už vyšlo i shrnutí problému (Alarm). Často se ale hledá způsob, co s tím jako člověk můžu dělat. Jedno z řešení je snížit svou uhlíkovou stopu. To je XXX.
+
+Jak změřit svou stopu? Pomocí uhlíkové kalkulačky. Odborníkem, který v Česku takovou kalkulačku dělá, je Viktor Třebický z CI2. ""
+
+Uhlíková stopa je (obdobně jako ekologická stopa) nepřímým ukazatelem spotřeby energií, výrobků a služeb. Měří množství skleníkových plynů, které odpovídají určité aktivitě či výrobku. Obvykle bývá vyjadřován v ekvivalentech CO2. Tedy nikoliv v hmotnosti uhlíku samotného, ale z něj vzniklého oxidu uhličitého a také emitovaných dalších skleníkových plynů (metanu, oxidu dusného, halogenovaných uhlovodíků), jejichž hmotnost je ale přepočítána na to, kolik CO2 by mělo týž oteplující účinek.
+
+CO JE VŮBEC TA UHLÍKOVÁ STOPA?
+PROČ JE POTŘEBA?
+JE UHLÍKOVÁ STOPA VĚC, KTERÁTO KLIMA OVLIVŇUJE HODNĚ?
+OBČAS SE ŘÍKÁ, ŽE OSOBNÍ VKLAD NEPOMŮŽE, ŽE JE POTŘEBA, ABY SE UHLÍKOVÉ STOPĚ VĚNOVALI HLAVNĚ POLITICI. JE TO TAK?
+CO TY DĚTI?
+
 <div id="klikatko"></div>
 <br>
-O tom, čeho všeho jsme ochotni se kvůli planetě vzdát, bude mimo jiné další Veřejná debata Plusu. V Papírně Plzeň budou ve středu 26.6. od 18:30 diskutovat Dana Drábová, Tomáš Klus, Marek Orko Vácha, Radim Tolasz a Aleš Michl. Přijít debatovat můžete také vy, případně poslouchejte přímý přenos na Plusu nebo sledujte videopřenos na Facebooku a webu."
-web: https://plus.rozhlas.cz/doba-konzumni-vzdame-se-pohodli-kvuli-planete-prijdte-debatovat-s-drabovou-7965959
-FB: https://www.facebook.com/events/1371778356304878/​
+
+Často se taky mluví o tom, že užitečné pro uhlíkovou stopu je vzdát s
+e dětí. Třebický je k tomu skeptický: "citace citace"
+
+O tom, čeho všeho jsme ochotni se kvůli planetě vzdát, bude mimo jiné další [Veřejná debata Plusu​](https://plus.rozhlas.cz/doba-konzumni-vzdame-se-pohodli-kvuli-planete-prijdte-debatovat-s-drabovou-7965959). V Papírně Plzeň budou ve středu 26.6. od 18:30 diskutovat Dana Drábová, Tomáš Klus, Marek Orko Vácha, Radim Tolasz a Aleš Michl. Přijít debatovat můžete také vy, případně poslouchejte přímý přenos na Plusu nebo sledujte videopřenos [na Facebooku](https://www.facebook.com/events/1371778356304878/) a webu.
--- js/script.js
+++ js/script.js
@@ -7,34 +7,39 @@ const uid = Date.now() + String(Math.round(Math.random() * 10000));
 
 const options = [
   {
-    text: "Jsem ochotný 2x týdně vynechat maso ze svého jídelníčku?",
+    text: "2× týdně vynechám maso ze svého jídelníčku.",
     id: 0,
-    save: 1300,
+    save: 290,
   },
   {
-    text: "Jsem ochotný v příštím roce vynechat létání (služební i osobní cesty)?",
+    text: "Vynechám létání (služební i osobní cesty).",
     id: 1,
-    save: 700,
+    save: 1379,
   },
   {
-    text: "Jsem ochotný jezdit do práce veřejnou dopravou, nikoliv autem?",
+    text: "Do práce budu jezdit do práce veřejnou dopravou, nikoliv autem.",
     id: 2,
-    save: 2300,
+    save: 890,
   },
   {
-    text: "Jsem ochotný začít využívat “zelenou elektřinu”?",
+    text: "Snížím teplotu vytápění svého domu/bytu o 1 °C.",
     id: 3,
-    save: 6994,
+    save: 40,
   },
   {
-    text: "Jsem ochotný třídit bioodpad?",
+    text: "Začnu využívat  „zelenou elektřinu“ vyrobenou z obnovitelných zdrojů.",
     id: 4,
-    save: 13,
+    save: 612,
   },
   {
-    text: "Jsem ochotný vzdát se 14-denní dovolené u moře v hotelu a nahradit ji pobytem v penzionu v ČR či SR?",
+    text: "Budu třídit biologicky rozložitelný odpad.",
     id: 5,
-    save: 820,
+    save: 135,
+  },
+  {
+    text: "Vzdám se 14denní dovolené u moře v hotelu a nahradím ji pobytem v penzionu v ČR či SR.",
+    id: 6,
+    save: 1844,
   },
 ];
 
@@ -42,70 +47,67 @@ class Klikatko extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      checkedIds: [],
+      checked: -1,
       sent: false,
-      result: 20000, // baseline
+      result: 5000, // baseline
     };
   }
 
-  selectData(id, save, event) {
-    const { checkedIds, result } = this.state;
-
-    const isSelected = event.currentTarget.checked;
-    if (isSelected) {
-      this.setState({
-        checkedIds: [...checkedIds, id],
-        result: result - save,
-      });
-    } else {
-      this.setState({
-        checkedIds: checkedIds.filter(item => id !== item),
-        result: result + save,
-      });
-    }
+  selectData(id, save) {
+    this.setState({
+      checked: id,
+      result: 5000 - save,
+    });
   }
 
   send() {
-    const { checkedIds } = this.state;
+    const { checked } = this.state;
     fetch("https://o2vm37ssm2.execute-api.eu-central-1.amazonaws.com/prod/uhlikova-stopa", {
       method: "POST",
       mode: "cors",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ id: uid, data: checkedIds }),
+      body: JSON.stringify({ id: uid, data: checked }),
     })
       .then(this.setState({ sent: true }));
   }
 
   render() {
-    const { checkedIds, sent, result } = this.state;
+    const { checked, sent, result } = this.state;
     return (
-      <div>
+      <div id="klikatko-app">
+        <div id="header">Kalkulačka snížení uhlíkové stopy</div>
         <form id="klikform">
-          <div>Čeho byste se byli nejvíc ochotní vzdát? Vyberte jednu až tři věci.</div>
+          <div>Průměrná roční stopa osobní spotřeby je <span className="large">5000 kg CO<sub>2</sub></span>. Vyberte, která cesta snížení uhlíkové stopy je pro vás nejpřijatelnější. Možnosti platí pro celý rok.</div>
+          <div className="small">Odhad platí pro průměrného městského člověka, který nežije nijak ekologicky, ale není ani velmi bohatý a neekologický.</div>
+          <hr className="snowfall" />
           {options.map(el => (
-            <div>
-              <span>{`${el.text} `}</span>
-              <input
-                type="checkbox"
-                checked={checkedIds.includes(el.id)}
-                onChange={e => this.selectData(el.id, el.save, e)}
-                disabled={(checkedIds.length >= 3 && !checkedIds.includes(el.id)) || sent}
-                name="select-data"
-              />
+            <div className="option">
+              <div className="option-input">
+                <input
+                  type="radio"
+                  checked={checked === el.id}
+                  id={`radio-${el.id}`}
+                  onChange={() => this.selectData(el.id, el.save)}
+                  disabled={sent}
+                  name="select-data"
+                />
+              </div>
+              <label className="option-text" htmlFor={`radio-${el.id}`}>{`${el.text} `}</label>
             </div>
           ))}
-          <button
-            type="button"
-            className={!sent ? "btn btn-primary" : "btn btn-sent"}
-            onClick={() => this.send()}
-            disabled={!(checkedIds.length > 0 && !sent)}
-          >
-            {!sent ? "Odeslat" : "Odesláno!"}
-          </button>
+          <hr className="snowfall" />
+          <div id="result">Vybranou cestou se Vaše uhlíková stopa v průměru sníží na <span className="large">{result} kg CO<sub>2</sub></span>, tedy o <span className="large">{Math.round(100-result/5000*100)} %</span>.</div>
+          <div id="button-flex">
+            <button
+              type="button"
+              className={!sent ? "btn btn-primary" : "btn btn-sent"}
+              onClick={() => this.send()}
+              disabled={checked === -1 || sent}
+            >
+              {!sent ? "Uložit" : "Uloženo!"}
+            </button>
+          </div>
         </form>
-        <div id="result">
-          {`Vaše uhlíková stopa je ${result}`}
-        </div>
       </div>
     );
   }
